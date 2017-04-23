12a05597caf3caf037e5c6d4134dac4095105806
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-23 07:46:13 +0200

    Adds dummy edit and trash icons

--- components/Card.js
+++ components/Card.js
@@ -4,6 +4,8 @@ import Panel from 'muicss/lib/react/panel'
 
 export default (props) => (
   <Panel>
+    <a href={props.data.url} className='mui--text-headline mui--pull-right material-icons' title='Delete' target='_blank'>delete</a>
+    <a href={props.data.url} className='mui--text-headline mui--pull-right material-icons' title='Edit' target='_blank'>mode_edit</a>
     <h1>{props.data.name}</h1>
     <p className='mui--text-headline'>{props.data.description}</p>
     <a href={props.data.url} className='mui--text-headline material-icons' title='Launch' target='_blank'>launch</a>
